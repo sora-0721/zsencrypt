35e8f327d71b20f78cf85590c74303b859474017
zeoseven <a16320080724@163.com>
2024-12-29 16:52:00 +0800

    u

--- components/main.js
+++ components/main.js
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 
 
 export const Header = () => {
-    const [tagid, setTagid] = useState('');
+    const [id, setId] = useState('');
     useEffect(() => {
-        let tagid = document.getElementById('tagid');
-        tagid && setTagid(tagid.textContent.trim());
+        const tagid = document.getElementById('tagid');
+        tagid && setId(tagid.textContent.trim());
     }, []);
     const s = 'md', b = 'lg';
     return (<>
@@ -23,28 +23,28 @@ export const Header = () => {
                     <div className={`mt-3 mb-2 px-4 mt-${s}-0 mb-${s}-0 px-${s}-0 px-${b}-5 collapse navbar-collapse`} id="daohanginfo">
                         <ul className="navbar-nav ms-auto">
                             <li className='nav-item'>
-                                <a className={`nav-link${tagid === 'home' ? ' active' : ''}`} href='/'>
+                                <a className={`nav-link${id === 'home' ? ' active' : ''}`} href='/'>
                                     首页
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link${tagid === 'apply' ? ' active' : ''}`} href='/apply/'>
+                                <a className={`nav-link${id === 'apply' ? ' active' : ''}`} href='/apply/'>
                                     申请证书
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link${tagid === 'manage' ? ' active' : ''}`} href='/manage/'>
+                                <a className={`nav-link${id === 'manage' ? ' active' : ''}`} href='/manage/'>
                                     证书管理
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className={`nav-link${tagid === 'doc' ? ' active' : ''}`} href='/documents/'>
+                                <a className={`nav-link${id === 'doc' ? ' active' : ''}`} href='/documents/'>
                                     文档
                                 </a>
                             </li>
                             <div className='d-flex'>
                                 <li className='nav-item'>
-                                    <a className={`nav-link${tagid === 'settings' ? ' active' : ''}`} href='/settings/'>
+                                    <a className={`nav-link${id === 'settings' ? ' active' : ''}`} href='/settings/'>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
                                             <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
                                             <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
@@ -52,8 +52,8 @@ export const Header = () => {
                                     </a>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className={`nav-link${tagid === 'updatelogs' ||
-                                        tagid === 'open' ? ' active' : ''
+                                    <a className={`nav-link${id === 'updatelogs' ||
+                                        id === 'open' ? ' active' : ''
                                         }`} href="#" data-bs-toggle="dropdown" aria-expanded="false">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-three-dots" viewBox="0 0 16 16">
                                             <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
@@ -61,8 +61,8 @@ export const Header = () => {
                                     </a>
                                     <ul className="dropdown-menu dropdown-menu-end">
                                         <div className="dropdown-div">
-                                            <li><a className={`dropdown-item${tagid === 'updatelogs' ? ' active' : ''}`} href="/info/UpdateLogs/">更新日志</a></li>
-                                            <li><a className={`dropdown-item${tagid === 'open' ? ' active' : ''}`} href='/info/open/'>开源</a></li>
+                                            <li><a className={`dropdown-item${id === 'updatelogs' ? ' active' : ''}`} href="/info/UpdateLogs/">更新日志</a></li>
+                                            <li><a className={`dropdown-item${id === 'open' ? ' active' : ''}`} href='/info/open/'>开源</a></li>
                                             <li><a className="dropdown-item" href='https://zeoseven.com/message/feedback/' target="_blank">错误反馈</a></li>
                                         </div>
                                     </ul>
