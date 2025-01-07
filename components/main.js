f2495be30233fc0f885d30327a93b8682449afb9
zeoseven <a16320080724@163.com>
2025-01-07 21:46:11 +0800

    u

--- components/main.js
+++ components/main.js
@@ -54,7 +54,7 @@ export const Header = () => {
                                 <li className="nav-item dropdown">
                                     <a href='javascript:;' className={`nav-link${id === 'updatelogs' ||
                                         id === 'open' ? ' active' : ''
-                                        }`} data-bs-toggle="dropdown" aria-expanded="false">
+                                        }`} data-bs-toggle="dropdown">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-three-dots" viewBox="0 0 16 16">
                                             <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
                                         </svg>
