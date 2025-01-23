e5f2607516af64600f0b014c16d24807075fcd1f
zeoseven <qh.1@outlook.com>
2025-01-23 15:33:59 +0800

    u

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import '@components/main.css';
 import { dTitle } from '@components/main';
 
@@ -49,7 +49,7 @@ export default function ({ Component, pageProps }) {
 
     useEffect(() => {
         window.innerWidth < mdBreakpoint && setNavbarDisplay("d-none");
-    }, [router.pathname])
+    }, [router.pathname]);
 
     useEffect(() => {
 
