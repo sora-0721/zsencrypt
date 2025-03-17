78846daee420ef89a1d89f75417b0f93077ccffa
zeoseven <a16320080724@163.com>
2025-03-17 20:20:09 +0800

    Document Optimization

--- pages/index.js
+++ pages/index.js
@@ -451,12 +451,14 @@ export default () => {
                             <a href="javascript:;" id="x-q-copy-textkey">复制 KEY</a>
                         </div>
                         <div className="col-12 mt-5">
-                            <p className="text-center"><button onClick={() => { window.location.reload() }} className="q-btn fw-bold">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
-                                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
-                                </svg><span className="ms-2">再申请一张！</span>
-                            </button></p>
+                            <p className="text-center">
+                                <button onClick={() => { window.location.reload() }} className="btn q-btn fw-bold">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
+                                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
+                                    </svg><span className="ms-2">再申请一张！</span>
+                                </button>
+                            </p>
                         </div>
                     </div>
 
