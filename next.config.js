de82d4534b9db00493e2771afe922f9aeaf230a8
Qingqiu Yao <qh.1@outlook.com>
2026-04-26 03:50:13 +0800

    tweak: rollback version display format

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
-export default async () => {
-
-    const repo = await fetch("https://api.github.com/repos/zeoseven/certple/git/refs/heads/main", {
-        method: "GET"
-    }).then(response => {
-        console.log("  fetch", response.status, response.url);
-        return response.json();
-    }).catch(e => console.log(e));
-
-    const GIT_HASH = repo?.object?.sha?.slice(0, 7) || "";
-
-    return {
-        output: "export",
-        trailingSlash: true,
-        devIndicators: false,
-        images: {
-            unoptimized: true
-        },
-        env: {
-            NEXT_PUBLIC_GIT_HASH: GIT_HASH
-        }
-    };
-
+export default {
+    output: "export",
+    trailingSlash: true,
+    devIndicators: false,
+    images: {
+        unoptimized: true
+    }
 };
