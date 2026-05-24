ade279e7ac8ffc9abff2b38ef0df5be4d000284d
Qingqiu Yao <qh.1@outlook.com>
2026-05-25 00:11:53 +0800

    tweak: modify 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,9 @@
+import { useEffect } from "react";
+
 export default function () {
-    return (<>
-        <div style={{
-            paddingTop: '20vh',
-            textAlign: 'center'
-        }}>
-            <h1 className="fw-bold">404 Not Found</h1>
-        </div>
-    </>);
+
+    useEffect(() => {
+        window.location.href = "/home/";
+    }, []);
+
 };
