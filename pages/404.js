c5fde159d69945ff5c8b08696b0c682da509faca
Qingqiu Yao <qh.1@outlook.com>
2026-05-25 03:11:07 +0800

    fix: add noindex to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect } from "react";
 
 export default function () {
@@ -5,5 +6,11 @@ export default function () {
     useEffect(() => {
         window.location.href = "/home/";
     }, []);
+
+    return (
+        <Head>
+            <meta name="robots" content="noindex" />
+        </Head>
+    );
 
 };
